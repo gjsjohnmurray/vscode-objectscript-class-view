ca5ebe1c60e316b8923153014a5106febb4e13d2
gjsjohnmurray <johnm@georgejames.com>
2023-01-28 00:20:32 +0000

    Drop unnecessary dependencies on Server Manager and Language Server

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## [0.0.4] 28-Jan-2023
+- Remove dependency on Language Server so we can publish on Open VSX Registry.
+- Remove dependency on Server Manager; connections with hardcoded credentials are possible.
+
 ## [0.0.3] 27-Jan-2023
 - Run on same minimum VS Code version (1.67) as extensions we depend on.
 
--- src/extension.ts
+++ src/extension.ts
@@ -8,17 +8,12 @@ import { AllClassMembersImplementationProvider } from './AllClassMembersImplemen
 export let cookiesCache: Cache;
 
 export let objectScriptApi: any;
-export let serverManagerApi: any;
 
 export async function activate(context: vscode.ExtensionContext) {
 
 	// Get the main extension exported API
 	const objectScriptExt = vscode.extensions.getExtension("intersystems-community.vscode-objectscript");
 	objectScriptApi = objectScriptExt?.isActive ? objectScriptExt.exports : objectScriptExt ? await objectScriptExt.activate() : undefined;
-
-	// Get the Server Manager extension exported API
-	const serverManagerExt = vscode.extensions.getExtension("intersystems-community.servermanager");
-	serverManagerApi = serverManagerExt?.isActive ? serverManagerExt.exports : serverManagerExt ? await serverManagerExt.activate() : undefined;
 
 	cookiesCache = new Cache(context, "cookies");
 
